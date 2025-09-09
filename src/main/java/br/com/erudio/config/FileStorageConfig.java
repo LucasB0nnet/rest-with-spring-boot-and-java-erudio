506ccb9c88e44bbd6b1fb27a65b3dc1dbe9598dd
LucasB0nnet <lucasbonnet688@gmail.com>
2025-09-09 18:06:08 -0300

    Dockerizing RestFull API Application

--- src/main/java/br/com/erudio/config/FileStorageConfig.java
+++ src/main/java/br/com/erudio/config/FileStorageConfig.java
@@ -2,7 +2,9 @@ package br.com.erudio.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Component
 @Configuration
 @ConfigurationProperties(prefix = "file")
 public class FileStorageConfig {
